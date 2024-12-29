52da4b7ffb924022f8be3dbf5bb82492331da890
Darrion Burgess <dargondab9@gmail.com>
2024-12-29 03:09:02 -0800

    not sure how much i love the current format but it works so ill commit here

--- src/todoist_sh/__init__.py
+++ src/todoist_sh/__init__.py
@@ -6,20 +6,74 @@ import argparse
 import keyring
 from keyrings.cryptfile.cryptfile import CryptFileKeyring
 
+
+def process_task_command(args):
+    if args.task_command == "list":
+        return get_tasks(TodoistAPI(get_api_key()))
+    else:
+        return print("nothing to do")
+
+
+def process_project_command(args):
+    if args.project_command == "list":
+        return get_projects(TodoistAPI(get_api_key()))
+    else:
+        return print("nothing to do")
+
+
 parser = argparse.ArgumentParser(
     prog="tdst", description="A simple todoist CLI", epilog="Get Things Done!"
 )
+subcommands = parser.add_subparsers(help="top-level subcommands", required=True)
+task_parser = subcommands.add_parser("tasks", help="get all tasks")
+task_argument = task_parser.add_argument(
+    "task_command",
+    choices=("list", "add", "update"),
+    help="the task command to run",
+)
+task_parser.set_defaults(func=process_task_command)
+
+project_parser = subcommands.add_parser("projects", help="get all tasks")
+project_argument = project_parser.add_argument(
+    "project_command",
+    choices=("list", "add", "update"),
+    help="the project command to run",
+)
+project_parser.set_defaults(func=process_project_command)
+
+key_parser = subcommands.add_parser("set-key", help="manually set api key")
+key_parser.add_argument("api_key", help="todoist api key")
 
 kr = CryptFileKeyring()
 
 
 def run():
-    api_key = get_api_key()
-    print(get_tasks(api_key))
+    args = parser.parse_args()
+    args.func(args)
+
+    # if args.tasks or args.projects is True:
+    #     api_key = get_api_key()
+    #     api = TodoistAPI(api_key)
+    #
+    #     if args.tasks is True:
+    #         tasks = get_tasks(api)
+    #         print(tasks)
+    #
+    #     if args.projects is True:
+    #         projects = get_projects(api)
+    #         print(projects)
+    #     else:
+    #         print("No arguments provided")
 
 
-def get_tasks(api_key):
-    api = TodoistAPI(api_key)
+def get_tasks(api):
+    try:
+        return api.get_tasks()
+    except Exception as e:
+        print(e)
+
+
+def get_projects(api):
     try:
         return api.get_tasks()
     except Exception as e:
